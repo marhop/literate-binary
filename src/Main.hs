fccb35ca90543cb016aeead60f4890cd84a2cec7
Martin Hoppenheit <martin@hoppenheit.info>
2018-10-10 22:07:00 +0200

    Add --plain CLI option

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.ByteString.Lazy as BL
 import Data.Semigroup ((<>))
 import Data.String.Conversions (cs)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import LiterateBinary (compile, markdownCode)
 import Options.Applicative
@@ -14,6 +15,7 @@ main = getOpts >>= runCompiler
 data Options = Options
     { optInput :: Maybe FilePath
     , optOutput :: Maybe FilePath
+    , optPlain :: Bool
     }
 
 -- | Parse command line options.
@@ -23,20 +25,32 @@ getOpts =
   where
     hdr = "lb - literate binary compiler"
     version = infoOption "1.0.0" (long "version" <> help "Show version number")
-    options = Options <$> input <*> output
+    options = Options <$> input <*> output <*> plain
     input =
         optional $
-        argument str (metavar "FILE" <> help "Input file (default STDIN)")
+        argument str (metavar "INPUT" <> help "Input file (default STDIN)")
     output =
         optional $
         strOption
-            (long "output" <> short 'o' <> metavar "FILE" <>
+            (long "output" <> short 'o' <> metavar "OUTPUT" <>
              help "Output file (default STDOUT)")
+    plain =
+        switch
+            (long "plain" <> short 'p' <> help "Input is just hex, no Markdown")
 
--- | Run LiterateBinary.comile function based on command line options.
+-- | Run LiterateBinary.compile function based on command line options.
 runCompiler :: Options -> IO ()
-runCompiler (Options i o) = do
-    text <- maybe TIO.getContents TIO.readFile i
-    case markdownCode text >>= compile of
-        Left err -> error $ cs err
-        Right bin -> maybe (BL.putStr bin) (`BL.writeFile` bin) o
+runCompiler opts = do
+    text <- readInput opts
+    either (error . cs) (writeOutput opts) $
+        if optPlain opts
+            then compile text
+            else markdownCode text >>= compile
+
+-- | Read input from a file or from STDIN.
+readInput :: Options -> IO T.Text
+readInput = maybe TIO.getContents TIO.readFile . optInput
+
+-- | Write output to a file or to STDOUT.
+writeOutput :: Options -> BL.ByteString -> IO ()
+writeOutput = maybe BL.putStr BL.writeFile . optOutput
