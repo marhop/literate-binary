5ce752cd6fc6e2d0a730a5ccf2c1440a8ea68acf
Martin Hoppenheit <martin@hoppenheit.info>
2021-10-31 22:14:00 +0100

    Add more tests for the hex parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,34 +15,52 @@ main = hspec $ do
     it "accepts an empty string" $
       parseHex "" `shouldBe` Right []
     it "accepts a simple hex string" $
-      parseHex "00ff" `shouldBe` Right [mkLiteral [0x00, 0xff]]
+      parseHex "00ff" `shouldBe` Right [lit [0x00, 0xff]]
     it "accepts a repetition" $
-      parseHex "(00){3}" `shouldBe` Right [Repetition [mkLiteral [0x00]] 3]
+      parseHex "(00){3}" `shouldBe` Right [Repetition [lit [0x00]] 3]
     it "accepts a repetition with suffix" $
       parseHex "(00){2K}"
-        `shouldBe` Right [Repetition [mkLiteral [0x00]] 2048]
+        `shouldBe` Right [Repetition [lit [0x00]] 2048]
     it "accepts an alternative" $
       parseHex "(00|ff|3333)"
         `shouldBe` Right
-          [Alternative $ fmap (pure . mkLiteral) [[0x00], [0xff], [0x33, 0x33]]]
+          [Alternative $ fmap (pure . lit) [[0x00], [0xff], [0x33, 0x33]]]
     it "accepts a range" $
-      parseHex "(00-ff)"
-        `shouldBe` Right [Range [mkLiteral [0x00]] [mkLiteral [0xff]]]
+      parseHex "(00-ff)" `shouldBe` Right [Range [lit [0x00]] [lit [0xff]]]
     it "accepts a dot range" $
       parseHex "." `shouldBe` Right [Byte]
     it "accepts a string literal in double quotes" $
-      parseHex "\"abc\"" `shouldBe` Right [mkLiteral [0x61 .. 0x63]]
+      parseHex "\"abc\"" `shouldBe` Right [lit [0x61 .. 0x63]]
     it "accepts a string literal in single quotes" $
-      parseHex "'abc'" `shouldBe` Right [mkLiteral [0x61 .. 0x63]]
+      parseHex "'abc'" `shouldBe` Right [lit [0x61 .. 0x63]]
     it "accepts a string with escape characters" $
-      parseHex "'\\''" `shouldBe` Right [mkLiteral [0x27]]
+      parseHex "'\\''" `shouldBe` Right [lit [0x27]]
     it "accepts combined macros like alternative + repetition" $
       parseHex "(00|ff){3}"
         `shouldBe` Right
-          [Repetition [Alternative [[mkLiteral [0x00]], [mkLiteral [0xff]]]] 3]
+          [Repetition [Alternative [[lit [0x00]], [lit [0xff]]]] 3]
+    it "accepts nested macros including whitespace" $
+      parseHex "00 (11 | (22|(33-44)|.){5} | 66) 77"
+        `shouldBe` Right
+          [ lit [0x00],
+            Alternative
+              [ [lit [0x11]],
+                [ Repetition
+                    [ Alternative
+                        [ [lit [0x22]],
+                          [Range [lit [0x33]] [lit [0x44]]],
+                          [Byte]
+                        ]
+                    ]
+                    5
+                ],
+                [lit [0x66]]
+              ],
+            lit [0x77]
+          ]
     it "accepts comments" $
       parseHex "ff # comment with # inside\n"
-        `shouldBe` Right [mkLiteral [0xff]]
+        `shouldBe` Right [lit [0xff]]
   describe "hex parser (invalid syntax)" $ do
     it "rejects non-hex characters" $
       parseHex "7f2x" `shouldSatisfy` isLeft
@@ -57,5 +75,5 @@ main = hspec $ do
     it "rejects incomplete ranges" $
       parseHex "(00-)" `shouldSatisfy` isLeft
 
-mkLiteral :: [Word8] -> HexString
-mkLiteral = Literal . pack
+lit :: [Word8] -> HexString
+lit = Literal . pack
